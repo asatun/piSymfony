196183f885b5cd31cc2b470eb0abdb404ef6526a
Manou11061987 <50183554+Manou11061987@users.noreply.github.com>
2020-06-21 20:37:15 +0200

    Gestion  CV+mise ajour

--- src/worktnBundle/Controller/DefaultController.php
+++ src/worktnBundle/Controller/DefaultController.php
@@ -1,13 +1,17 @@
 <?php
 
-namespace worktnBundle\Controller;
+namespace WorktnBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 class DefaultController extends Controller
 {
+    /**
+     * @Route("/")
+     */
     public function indexAction()
     {
-        return $this->render('worktnBundle:Default:index.html.twig');
+        return $this->render('WorktnBundle:Default:index.html.twig');
     }
 }
